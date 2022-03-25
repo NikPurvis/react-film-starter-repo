937f25bcd2376509ba8029858d6871b72479bf19
NikPurvis <nikpurvis@gmail.com>
2022-03-24 18:07:57 -0600

    Up to Step 6

--- src/FilmList.js
+++ src/FilmList.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react"
+import TMDB from "./TMDB"
+// import TMDB from "./TMDB"
 
 export default class FilmList extends Component {
 
     render () {
 
-        return (
+        const allFilms = TMDB.films.map((film, index) => (
+            <div class="film-row">
+                <h1>{film.title}</h1>
+            </div>
+        ))
 
-        <div className="film-list">
-            <h1 className="section-title">FILMS</h1>
-        </div>
-        
+        return (
+            <div className="film-list">
+                <h1 className="section-title">FILMS</h1>
+                {allFilms}
+            </div>
         )
     }
     
